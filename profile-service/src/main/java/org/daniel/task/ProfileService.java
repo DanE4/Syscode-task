4ec4e2adde16d0757adfae7a2d828a121edcf390
DanE4 <90055044+DanE4@users.noreply.github.com>
2024-01-27 00:30:05 +0100

    Init

---
+++ profile-service/src/main/java/org/daniel/task/ProfileService.java
@@ -0,0 +1,11 @@
+package org.daniel.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileService {
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileService.class, args);
+	}
+}

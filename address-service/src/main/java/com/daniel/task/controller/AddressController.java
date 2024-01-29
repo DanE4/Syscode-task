26e4363cd04ea8468bb620b2aba9f7f23e65fae7
DanE4 <90055044+DanE4@users.noreply.github.com>
2024-01-29 22:32:42 +0100

    feat: new endpoint for Address Service, what returns a random Address obj

--- address-service/src/main/java/com/daniel/task/controller/AddressController.java
+++ address-service/src/main/java/com/daniel/task/controller/AddressController.java
@@ -1,26 +1,41 @@
 package com.daniel.task.controller;
 
+import com.daniel.task.Response;
+import com.daniel.task.entity.Address;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/address")
 public class AddressController {
-    @GetMapping("/{id}")
-    public ResponseEntity<String> getAddress(@PathVariable UUID id) {
-        //TODO Needs to be implemented
-        String address = "";
-        if (address != null) {
-            return ResponseEntity.ok(address);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+
+    //Task: "Legyen benne egy végpont, ami visszaad egy Address objektumot, aminek két mezöje van: id, address"
+    @Operation(summary = "Get a random address", description = "Fetch a random address", tags = {"Address"})
+    @GetMapping("/")
+    public ResponseEntity<Response> getAddress() {
+
+        //! because we don't need a database, therefore I am generating a random UUID, and I am not using a mapper and
+        //! a service, and the other not needed things for this simple example
+        log.info("Getting address");
+        UUID uuid = UUID.randomUUID();
+        Address address = Address.builder()
+                .id(uuid)
+                .address("1234 Random St")
+                .build();
+        log.info("Address: " + address.getAddress());
+        return ResponseEntity.ok(Response.builder()
+                .status(HttpStatus.OK.value())
+                .data(address)
+                .build());
     }
 }

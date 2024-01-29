657d480e57bcb1ef51d41d100ba6c26413b75dfd
DanE4 <90055044+DanE4@users.noreply.github.com>
2024-01-29 22:28:14 +0100

    feat: Swagger generation and Response model added

---
+++ address-service/src/main/java/com/daniel/task/OpenApiConfig.java
@@ -0,0 +1,52 @@
+package com.daniel.task;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "DanE",
+                        email = "danieleros.business@gmail.com",
+                        url = "https://google.com"
+                ),
+                description = "OpenApi documentation for Syscode Task, Address Service",
+                title = "Address Service - OpenApi specification",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://some-url.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8081"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "http://localhost:8081"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "basicAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic"
+)
+
+public class OpenApiConfig {
+}
+
---
+++ profile-service/src/main/java/org/daniel/task/model/Response.java
@@ -0,0 +1,19 @@
+package org.daniel.task.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Response {
+    int status;
+    Object data;
+    String error;
+}
+
+
+

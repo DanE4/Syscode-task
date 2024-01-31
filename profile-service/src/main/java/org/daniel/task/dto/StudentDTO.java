3e4cd5c676f344695afa151793b3af6d624e971f
DanE4 <90055044+DanE4@users.noreply.github.com>
2024-01-31 08:26:53 +0100

    fix: name size on StudentDTO is now validated

--- profile-service/src/main/java/org/daniel/task/dto/StudentDTO.java
+++ profile-service/src/main/java/org/daniel/task/dto/StudentDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 public class StudentDTO {
     @JsonProperty("name")
     @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 30, message = "Name must be between 2 and 30 characters")
     @Schema(description = "Name", example = "Jane Doe")
     private String name;
 
